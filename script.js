c200e3dfdef5ffb90b0844d127d5801a29637ccd
shadik <shadikmahamudsohan@gmail.com>
2022-07-16 22:41:09 +0600

    jump changed and speed up added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const player = document.getElementById("player");
 const block = document.getElementById("block");
 const counter = document.getElementById("counter");
 var count = 0;
+let animationTime = parseFloat(getComputedStyle(block).animationDuration);
 
 function jump() {
     if (player.classList == "jump-animation") {
@@ -10,7 +11,7 @@ function jump() {
     player.classList.add("jump-animation");
     setTimeout(function () {
         player.classList.remove("jump-animation");
-    }, 1000);
+    }, 700);
 }
 
 const checkDead = setInterval(function () {
@@ -23,9 +24,16 @@ const checkDead = setInterval(function () {
         const playAgain = confirm("Do you want to play again?");
         if (playAgain) {
             count = 0;
+            animationTime = 2.5;
+            block.style.animation = `move 2.5s infinite linear`;
             block.classList.add("move-animation");
         }
     } else {
         count++;
+        console.log(animationTime);
+        if (animationTime > 1.7) {
+            animationTime = animationTime - 0.0001;
+            block.style.animation = `move ${animationTime}s infinite linear`;
+        }
     }
 }, 10);
